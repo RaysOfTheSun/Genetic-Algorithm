8c0ce1f6b5b92f047662652a7c6d53ac62c78c09
RaysOfTheSun <rdelsol35@gmail.com>
2018-04-19 22:16:46 +0800

    add method for fitness evaluation in the samples class

--- Samples.py
+++ Samples.py
@@ -8,11 +8,20 @@ class Samples:
         :param mutation_rate: The probability that a member of the population is mutated
         :param target: The target phrase that will be the basis of the base population
         """
-        self.pool = []
+        self.dna_pool = []
         self.mutation_rate = mutation_rate
         self.target = target
         self.max_fitness = 0
         self.mating_pool = []
 
-        for generation in range(cap):
-            self.pool.append(DNA(len(target)))
+        for element in range(cap):
+            self.dna_pool.append(DNA(len(target)))
+
+    def evaluate_fitness(self):
+        """Evaluates the fitness of each element in the DNA pool"""
+        for dna in self.dna_pool:
+            dna.evaluate_fitness(self.target)
+            if self.max_fitness == 0:
+                self.max_fitness = dna.fitness
+            elif self.max_fitness < dna.fitness:
+                self.max_fitness = dna.fitness
