b4d39a5d7bb57e111702e3099fab0834b720f7c7
RaysOfTheSun <rdelsol35@gmail.com>
2018-04-21 11:31:11 +0800

    update Samples and DNA class
    + applied name mangling to attributes of the Samples class
    + the crossover method in the DNA class now checks if the parents are the same and takes proper action

--- Samples.py
+++ Samples.py
@@ -10,52 +10,55 @@ class Samples:
         :param target: The target phrase who's length will be the basis of the creation
         of the initial sample population
         """
-        self.dna_pool = []
-        self.mating_pool = []
-        self.mutation_rate = mutation_rate
-        self.target = target
-        self.max_fitness = 0
-        self.cap = cap
-        self.generation = 0
-        self.fittest = ""
-        self.average_fitness = 0
+        self.__dna_pool = []
+        self.__mating_pool = []
+        self.__mutation_rate = mutation_rate
+        self.__target = target
+        self.__max_fitness = 0
+        self.__cap = cap
+        self.__generation = 0
+        self.__fittest = ""
+        self.__average_fitness = 0
 
         for element in range(cap):
-            self.dna_pool.append(DNA(len(target)))
+            self.__dna_pool.append(DNA(len(target)))
 
     def evaluate_fitness(self):
         """Evaluates the fitness of each element in the DNA pool"""
-        self.average_fitness = 0
-        for dna in self.dna_pool:
-            dna.evaluate_fitness(self.target)
-            self.average_fitness += dna.fitness
-            if self.max_fitness <= dna.fitness:
-                self.max_fitness = dna.fitness
-                self.fittest = dna.code
-        self.average_fitness = int((self.average_fitness / self.cap))
+        self.__average_fitness = 0
+        for dna in self.__dna_pool:
+            dna.evaluate_fitness(self.__target)
+            self.__average_fitness += dna.fitness
+            if self.__max_fitness <= dna.fitness:
+                self.__max_fitness = dna.fitness
+                self.__fittest = dna.code
+        # Complete the computation of the average fitness for this generation
+        self.__average_fitness = int((self.__average_fitness / self.__cap))
 
     def build_mating_pool(self):
-        self.mating_pool = []
-        for dna in self.dna_pool:
+        self.__mating_pool = []
+        for dna in self.__dna_pool:
             if dna.fitness != 0:
                 for x in range(dna.fitness):
-                    self.mating_pool.append(dna)
+                    self.__mating_pool.append(dna)
 
     def evolve(self):
-        self.dna_pool = []
-        for value in range(self.cap):
-            mother = random.choice(self.mating_pool)
-            father = random.choice(self.mating_pool)
+        self.__dna_pool = []
+        for value in range(self.__cap):
+            mother = random.choice(self.__mating_pool)
+            father = random.choice(self.__mating_pool)
+            while mother == father:
+                mother = random.choice(self.__mating_pool)
             child = mother.crossover(father)
-            child.mutate(self.mutation_rate)
-            self.dna_pool.append(child)
+            child.mutate(self.__mutation_rate)
+            self.__dna_pool.append(child)
 
     def evaluate(self)->bool:
         print("generation: {}; fittest: {}; Fitness: {}%; Average Fitness: {}%; MR: {}%".format(
-            self.generation, self.fittest, self.max_fitness, self.average_fitness, self.mutation_rate*100
+            self.__generation, self.__fittest, self.__max_fitness, self.__average_fitness, self.__mutation_rate * 100
         ))
-        if self.fittest == self.target:
+        if self.__fittest == self.__target:
             return False
 
-        self.generation += 1
+        self.__generation += 1
         return True
